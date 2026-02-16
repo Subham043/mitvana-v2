1ed3741f14c3b3e4ced2e8878df48dcaa7cfcdf0
Subham043 <sahasubham043@gmail.com>
2026-02-16 21:27:35 +0530

    changed the frontend ports in backend env

--- mitvana-admin/vite.config.ts
+++ mitvana-admin/vite.config.ts
@@ -5,7 +5,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vite.dev/config/
 export default defineConfig({
   server: {
-    port: 3002,
+    port: 3001,
     open: true,
   },
   plugins: [
