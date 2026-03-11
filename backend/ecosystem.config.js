be6783b08937bbf23697e3ae6641ef1855ba7120
Subham043 <sahasubham043@gmail.com>
2026-03-11 23:23:28 +0530

    added pm2 setup

---
+++ backend/ecosystem.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  apps: [
+    {
+      name: 'mitvana_api',
+
+      script: './dist/main.js',
+
+      instances: 'max', // use all CPU cores
+      exec_mode: 'cluster',
+
+      watch: false,
+
+      autorestart: true,
+
+      max_memory_restart: '1G',
+
+      port: 8000,
+
+      time: true,
+    },
+  ],
+};
--- mitvana-admin/src/config/env.production.ts
+++ mitvana-admin/src/config/env.production.ts
@@ -1,7 +1,7 @@
 import { base } from "./env.base";
 
-const APP_ENDPOINT = "https://portal.aaaedu.in";
-const API_ENDPOINT = "https://server-api.aaaedu.in";
+const APP_ENDPOINT = "https://mitvana-admin.digisole.in";
+const API_ENDPOINT = "https://mitvana-api.digisole.in";
 
 /*
  * Configuration for production env
